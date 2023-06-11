587bdf4d28d094e7f261180b5aea3f843fe497be
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-11 11:01:21 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+<h3>LOGIN</h3>
+<form id ="login" action="logado.php" method="POST" >
+    Login: <input type="text" name="login" required><br>
+    Senha: <input type="password" name="senha" required><br><br>
+    <input type="submit" id="entrar" value="Entrar">
+</form>
+</center>
+</body>
+</html>
