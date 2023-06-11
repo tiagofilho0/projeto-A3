0685d1f368dbb126f070c71ed6ed11999cb38769
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-11 11:04:17 -0300

    Create cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+
+    <form id = "cadastar" action="cadastro.php" method="post">
+       
+        nome:  <input type = "text" name = "nome" ><br>
+        login: <input type = "text" name = "login" ><br>
+        senha: <input type = "password" name = "senha" ><br> 
+               <input type = "submit" id = "cadastrar" value = "cadastrar">
+
+    </center>
+</body>
+</html>
