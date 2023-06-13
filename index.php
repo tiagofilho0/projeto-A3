d4f136054d6bb3f18c164a7b1c6514b01512c360
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-13 00:17:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php
-
 session_start();
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <center>
-
-  Olá, <?php if(isset($_SESSION['nome']) == null) { ?> visitante<br>
-        
-      Realize o <a href="login.php">login</a>
-        
-        <?php } else { echo $_SESSION['nome']; ?>
+</html>
+<style>
+      body{
+        font-family: Arial, Helvetica, sans-serif;
+        background-image: linear-gradient(45deg,cyan, yellow);
+      }
+      div{
+        background-color: rgba(0, 0, 0,0.9);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        padding: 80px;
+        border-radius: 15px;
+        color: white;
+      }
        
-        <br><a href="cadastrar.php">cadastrar novo usuario</a><br>
-            <a href="logout.php">sair</a>
-        
-        <?php } ?>
+      input{
+        padding: 15px;
+        border: none;
+        outline: none;
+        font-size: 15px;
+      }
+    </style>
+  <body> 
+    <div>
+     olá, seja bem vindo <?php
+     if (isset($_SESSION['nome']) == null){
+        ?> visitante<br>
+    Realize o <a href="login.php">login</a>
+     <?php } else {
+        echo $_SESSION['nome']; ?><br><br>
+    para cadastrar um novo usuario clique em: <a href="cadastrar.php"><h3>cadastrar</h3></a>
+    para finalizar sessão clique em: <a href="logout.php"><h3>sair</h3></a>
+    <?php } ?>
+     </div>
 
-   </center>
 </body>
 </html>
