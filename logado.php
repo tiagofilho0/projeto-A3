d176731587e6ebae6fbe2d4822e286a575d3573f
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-13 00:17:40 -0300

    Update logado.php

--- logado.php
+++ logado.php
@@ -2,24 +2,20 @@
 <?php
 
 include('conexao.php');
-$login = isset($_POST['login'])? $_POST['login'] : '';
-$senha = isset($_POST['senha'])? $_POST['senha'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-$select = "SELECT nome,login,senha from login where login = '$login' and senha = '$senha'";
-
-$execute = mysqli_query($conexao,$select);
-
+$select = "SELECT nome, login, senha from login where login = '$login' and senha = '$senha' " ;
+$execute = mysqli_query($conexao, $select);
 $dados = mysqli_fetch_row($execute);
 
-
 if($login == $dados[1] && $senha == $dados[2]){
-    
     session_start();
-    $_session['nome'] = $dados[0];
-
-    header('location: index.php');
+    $_SESSION['nome'] = $dados[0];
+   header('location: index.php');
 
 }else{
-header('location: login.php'); }
+    header('location: login.php');
+}
 
 ?>
