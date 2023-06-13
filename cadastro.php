5f11398a3df31c451a8304210530cbd2c46db2e4
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-13 00:15:58 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,19 +1,14 @@
-cadastro.php
-
 <?php
 
-include('conexao.php');
+include("conexao.php");
 
-$nome = isset($_POST['nome'] )? $_POST['nome'] : '' ;
-$login = isset($_POST['login'] )? $_POST['login'] : '' ;
-$senha = isset($_POST['senha'] )? $_POST['senha'] : '' ;
+$nome = isset($_POST['nome']) ? $_POST ['nome'] : '';
+$login = isset($_POST['login']) ? $_POST ['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST ['senha'] : '';
 
-$insert = "INSERT INTO
-           login (nome,login,senha)
-           VALUES('$nome','$login','$senha');";
-
-$query = mysqli_query($conexao,$insert);
-
-header('location:index.php');
+$insert = "INSERT INTO login (nome, login, senha)
+          VALUES('$nome', '$login', '$senha')";
+$query = mysqli_query($conexao, $insert);
+header ('location: index.php');
 
 ?>
