80aaa8b9f3886af77c3ad62a2b2c094aaf7bcad7
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-16 20:07:04 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de login</title>
-    <style>
+    <title>Document</title>
+</head>
+<style>
       body{
         font-family: Arial, Helvetica, sans-serif;
-        background-image: linear-gradient(45deg,cyan, yellow);
+        background-image: linear-gradient(45deg,black, white);
       }
       div{
         background-color: rgba(0, 0, 0,0.9);
@@ -28,18 +29,14 @@
         font-size: 15px;
       }
     </style>
-</head>
 <body>
-    
-<div>   <h1>LOGIN</h1>
-        <form name="login" action="logado.php" method="POST">
-        Login: <input type="text" name="login" required placeholder="digite seu login">
-        <br><br>
-        Senha:<input type="password" name="senha" required placeholder="Digite sua senha">
-        <br><br>
-        <input type="submit" id="entrar" value="Entrar">
-</div>
-    <form>
-    
+    <div>
+        <h2>LOGIN</h2>
+        <form id="login" action="logado.php" method="POST">
+            Login: <input type="text" name="login" required><br><br>
+            Senha:<input type="password" name="senha" required><br><br>
+            <input type="submit" id="entrar" value="Entrar">
+        </form>
+    </div>
 </body>
 </html>
