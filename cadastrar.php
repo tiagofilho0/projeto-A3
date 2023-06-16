52d28025f8c2e064d2d63dfb3df26cf2b96b8bf3
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-16 20:04:06 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,38 +1,22 @@
 
-<html>
-<style>
-      body{
-        font-family: Arial, Helvetica, sans-serif;
-        background-image: linear-gradient(45deg,cyan, yellow);
-      }
-      div{
-        background-color: rgba(0, 0, 0,0.9);
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        padding: 80px;
-        border-radius: 15px;
-        color: white;
-      }
-       
-      input{
-        padding: 15px;
-        border: none;
-        outline: none;
-        font-size: 15px;
-      }
-    </style>
-    <body>
-        <div>
-        <h3>Digite as seguintes informações</h3>
-<form id="cadastro" action="cadastro.php" method="post">
-    nome: <input type="text" name="nome" required placeholder="Digite seu nome"><br><br>
-    login : <input type="text" name="login" required placeholder="Digite seu login"><br><br>
-    senha: <input type="password" name="senha" required placeholder="Digite sua senha"><br><br>
-    <input type="submit" id="cadastrar" valve="cadastrar">
-
-    </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
 <body>
-    <html>
+<center>
+
+<form id = "cadastro" action = "cadastro.php" method = "POST">
+    nome: <input type = "text" name = "nome" required><br>
+    login: <input type = "text" name = "login" required><br>
+    senha: <input type = "pasword" name = "senha" required><br><br>
+    <input type = "submit" id = "cadastrar" value = "cadastrar">
+
+</center>
+</body>
+</html>
