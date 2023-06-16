e29abf8881db842316b96ce697a124f3d3b04cd7
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-06-16 20:05:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,7 @@
 
 <?php
-
-$conexao = mysqli_connect('localhost', 'root','', 'projetoa3', '3306');
-
-if(!$conexao){
-   die("erro de conexão");
+$conexao = mysqli_connect('localhost', 'root', '', 'projetoa3', '3306');
+if (!$conexao) {
+    die("Erro de conexão: " . mysqli_connect_error());
 }
 ?>
